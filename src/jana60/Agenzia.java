54c37095b94c06ac34ae328c15896f586deba4ca
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-27 13:23:09 +0200

    Corretta struttura menu selezione

--- src/jana60/Agenzia.java
+++ src/jana60/Agenzia.java
@@ -9,35 +9,43 @@ public class Agenzia {
 		Scanner scan = new Scanner(System.in);
 
 		boolean continua = true;
+		boolean creaVacanza = true;
+		
+		System.out.println("Benvenuto, grazie per averci scelto.");
 		
 		do //Ciclo di selezione dove l'utente decide se creare un biglietto o chiudere la procedura.
 		{
-			
-			System.out.println("Benvenuto, grazie per averci scelto.\nVuoi creare un biglietto?");
+			System.out.println("Vuoi prenotare una nuova Vacanza?");
 			System.out.println("1- SI, 2-NO");
 			
 			String input = scan.nextLine();
-			if (input.equals("1"))		//L'utente vuole continuare
+			
+			switch (input)
 			{
-				continua = true;
+				case "1":
+					continua = true;
+					creaVacanza = true;
+					break;
+					
+				case "2":
+					continua = true;
+					creaVacanza = false;
+					break;
+					
+				default:
+					continua = false;
+					System.out.println("Attenzione, hai inserito un valore non valido.");
+					break;
 			}
-			else if (input.equals("2"))	//L'utente vuole annullare la procedura
-			{
-				continua = false;
-				System.out.println("Va bene, alla prossima!");
-				scan.close();			//Chiusura dello scan prima del termine dell'applicazione
-				break;
-			}
-			else
-			{
-				continua = false;
-				System.out.println("Attenzione, hai inserito un valore non valido.");	
-			}
-				
 		}
 		while (!continua);
 		
+		while (creaVacanza) 
+		{
+			
+		}
 		
+		System.out.println("Va bene, alla prossima!");
 		
 		
 		scan.close();
